a972b6d72905c038ea6528408a3dccd2d4952437
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-08-24 13:57:02 +1000

    Updated index.js dependencies and connected to DB

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import { user } from "pg/lib/defaults";
+
+const { Pool } = require("pg");
+const inquirer = require("inquirer");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: "localhost",
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+});
+
+pool.connect();
+
